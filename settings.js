bf9f7db9741778836ac36905cc431a075a20bc08
bowser207 <109121000+bowser207@users.noreply.github.com>
2023-06-21 09:52:05 -0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+function back()
+{
+    window.location = "index.html";
+}
+
+function blue()
+{
+    document.getElementById("background").style.backgroundColor = "blue";
+}
+
+function green()
+{
+    document.getElementById("background").style.backgroundColor = "green";
+}
+
+function white()
+{
+    document.getElementById("background").style.backgroundColor = "white";
+}
+
+function red()
+{
+    document.getElementById("background").style.backgroundColor = "red";
+}
